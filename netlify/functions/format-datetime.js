afe22ae870c0854b202c4064110b0210813c49b2
hamzanaseemdeploy-dot <hamzanaseemdeploy@gmail.com>
2026-01-18 01:45:09 +0430

    Update format-datetime.js

--- netlify/functions/format-datetime.js
+++ netlify/functions/format-datetime.js
@@ -5,30 +5,27 @@ exports.handler = async (event) => {
   
   let appointmentDateTime, appointmentDate, appointmentTime, timeMin, timeMax;
 
-  // The timezone is always -04:00 for New York
-  const fixedTimezoneOffset = '-04:00';
-
   // Check if the appointment datetime string ends with 'T00:00:00' to identify a date-only input.
   if (appointment_datetime.endsWith('T00:00:00')) {
     // Case 2: appointment_datetime is effectively a date-only input
     const appointmentDateOnly = appointment_datetime.split('T')[0];
 
-    appointmentDateTime = `${appointmentDateOnly}T00:00:00${fixedTimezoneOffset}`;
+    appointmentDateTime = `${appointmentDateOnly}T00:00:00`;
     appointmentDate = appointmentDateOnly;
     appointmentTime = "";
-    timeMin = `${appointmentDateOnly}T00:00:00${fixedTimezoneOffset}`;
-    timeMax = `${appointmentDateOnly}T23:59:59${fixedTimezoneOffset}`;
+    timeMin = `${appointmentDateOnly}T00:00:00`;
+    timeMax = `${appointmentDateOnly}T23:59:59`;
 
   } else {
-    // Case 1: appointment_datetime includes a specific time and timezone
+    // Case 1: appointment_datetime includes a specific time
     const apptDt = new Date(appointment_datetime);
     const timeMaxDt = new Date(apptDt.getTime() + durationInMinutes * 60000);
 
-    appointmentDateTime = `${appointment_datetime}${fixedTimezoneOffset}`;
+    appointmentDateTime = appointment_datetime;
     appointmentDate = appointment_datetime.split('T')[0];
-    appointmentTime = appointment_datetime.split('T')[1].slice(0, 8); // Extracts 'HH:mm:ss'
-    timeMin = `${appointment_datetime}${fixedTimezoneOffset}`;
-    timeMax = `${timeMaxDt.toISOString().slice(0, 19)}${fixedTimezoneOffset}`;
+    appointmentTime = appointment_datetime.split('T')[1].slice(0, 8); // 'HH:mm:ss'
+    timeMin = appointment_datetime;
+    timeMax = timeMaxDt.toISOString().slice(0, 19);
   }
 
   const responseBody = {
